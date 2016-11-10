ada0923a392ae7fba81731562dba6407bfeba0b4
Adam Mitro <Arm148@pitt.edu>
2016-11-10 18:21:09 -0500

    Adding updated php class files and css

--- ParentClass.php
+++ ParentClass.php
@@ -1,6 +1,32 @@
 <?php
 	// This is the file for the parent class
 
-	class ParentClass {
-		
-	}
+	class List{
+	  
+	  private $item1;
+    private $item2;
+    private $item3;
+    
+		public function __construct($item1, $item2, $item3){
+		  $this->item1 = $item1;
+      $this->item2 = $item2;
+      $this->item3 = $item3;
+    }
+    
+    public function getFirstItem(){
+      return $this->item1;
+    }
+    
+    public function getSecondItem(){
+      return $this->item2;
+    }
+    
+    public function getThirdItem(){
+      return $this->item3;
+    }
+    
+    public function __toString(){
+      $string = "I have a list with " . $this->item1 . ", " . $this->item2 . ", and " . $this->item3;
+      return $string;
+    }
+  }
--- assignment3.php
+++ assignment3.php
@@ -1,5 +1,8 @@
-<?php 
-
+<?php
+include 'ParentClass.php';
+include 'ChildClass.php';
+$grocery_list = new ParentClass("Eggs", "Milk", "Bread");
+$updated_list = new ChildClass("Lucky Charms", "Chips", "Cheese");
 	
 
 ?>
@@ -9,10 +12,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Assignment 3</title>
-
 	<link rel="stylesheet" href="assignment3.css">
 </head>
 <body>
-	
+	<div class="list">
+		<ul>
+			<li> <?= $grocery_list ?> </li>
+			<li> <?= $updated_list ?> </li>
+		</ul>
+	</div>
 </body>
 </html>
